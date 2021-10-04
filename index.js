481fae8439ea28810db0d3b3451e517fac4958b1
Cole Sibold <csibold@gmail.com>
2021-10-04 12:37:50 -0400

    Homework Week 2 finished

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     var imageArray = [
         "https://www.cadogantate.com/fine-art-paintings/images/Adoration%20of%20the%20Magi.jpg",
         "https://www.cadogantate.com/fine-art-paintings/images/Madonna%20and%20child%20in%20a%20niche.jpg",
+        "https://www.cadogantate.com/fine-art-paintings/images/Annunciation.jpg",
+        "https://www.cadogantate.com/fine-art-paintings/images/St%20Luke%20Painting%20the%20Virgin.jpg",
+        "https://www.cadogantate.com/fine-art-paintings/images/The%20Agony%20of%20the%20Garden.jpg",
 
 
     ];
@@ -11,26 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
     var rightButton = document.querySelector("#rightButton");
     var imageHolder = document.getElementById("imageHolder");
 
-    //src = imageArray[0]
+    
     imageHolder.src = imageArray[imageArrayIndex];
 
     rightButton.addEventListener('click', function () {
         if (imageArrayIndex < imageArray.length - 1) {
-            imageArrayIndex++; //increment 0 to 1
-            imageHolder.src = imageArray[imageArrayIndex]; // src = imageArray[1]
+            imageArrayIndex++;
+            imageHolder.src = imageArray[imageArrayIndex];
         }
         else {
-            console.log("Out of Bounds on right button!")
+            console.log("Too big")
         }
     })
 
     leftButton.addEventListener('click', function () {
         if (imageArrayIndex > 0) {
-            imageArrayIndex--; //increment 1 to 0
-            imageHolder.src = imageArray[imageArrayIndex]; // src = imageArray[0]
+            imageArrayIndex--; 
+            imageHolder.src = imageArray[imageArrayIndex]; 
         }
         else {
-            console.log("Out of Bounds on right button!")
+            console.log("Too big")
         }
     })
 })
